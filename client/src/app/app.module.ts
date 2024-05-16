ac38cd23c69e240fe41950508c3a761c8f6dbb20
NansikaChristine <159410999+NansikaChristine@users.noreply.github.com>
2024-05-16 17:28:50 +0530

    End Of Section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
@@ -30,8 +30,7 @@ import { SharedModule } from './_modules/shared.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule, 
+    FormsModule, 
     BrowserAnimationsModule,
     SharedModule
   ],
