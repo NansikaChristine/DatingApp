c331a801d85599d16f97e326312430674692cb6c
NansikaChristine <159410999+NansikaChristine@users.noreply.github.com>
2024-05-18 23:07:02 +0530

    End Of Section 7

--- client/src/app/errors/not-found/not-found.component.ts
+++ client/src/app/errors/not-found/not-found.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+
+  constructor(private router: Router){
+  }
+
+  ngOnInit(): void {
+  }
 
 }
--- client/src/app/errors/server-error/server-error.component.ts
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -15,7 +15,6 @@ export class ServerErrorComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    
   }
 
 }
